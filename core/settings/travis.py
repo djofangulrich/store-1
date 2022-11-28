9b372afb8b4855a9d93374645507e991e7cea035
djofang99 <djofangulrich05@gmail.com>
2022-11-28 19:50:01 +0100

    add travis files

---
+++ core/settings/travis.py
@@ -0,0 +1,15 @@
+from . import *
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
